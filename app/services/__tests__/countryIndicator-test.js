87227e93fa7d8897acd25ce3f4840b601814daec
joshweir <joshua.weir@gmail.com>
2018-04-15 10:24:31 +1000

    add service test

---
+++ app/services/__tests__/countryIndicator-test.js
@@ -0,0 +1,42 @@
+import createRestApiClient from '../../utils/createRestApiClient';
+import createCountryIndicatorService from '../../services/countryIndicator';
+
+jest.mock('../../utils/createRestApiClient');
+
+describe('countryIndicator service', () => {
+  const mockApi = () => {
+    const spy = jest.fn().mockImplementation(() => Promise.resolve({
+      data: 'request response'
+    }));
+    createRestApiClient.mockImplementation(() => {
+      return {
+        withConfig: () => ({request: spy})
+      };
+    });
+    return spy;
+  };
+
+  describe('#getCountryIndicator', () => {
+    test('makes a client request to the country indicator data endpoint',
+    (done) => {
+      const params = {
+        country: 'united-states',
+        indicator: 'gdp',
+        releaseDateBefore: '2018-01-01',
+        perPage: 20
+      };
+      const getCountryIndicatorSpy = mockApi();
+      createCountryIndicatorService().getCountryIndicator(params)
+      .then(() => {
+        expect(getCountryIndicatorSpy)
+        .toHaveBeenCalledWith({
+          method: 'GET',
+          params,
+          url: '/country-indicator-data'
+        });
+        expect(getCountryIndicatorSpy).toHaveBeenCalledTimes(1);
+        done();
+      });
+    });
+  });
+});
