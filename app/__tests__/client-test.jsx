69607af86588a132233dcda5d2cb02e64052a0d8
joshweir <joshua.weir@gmail.com>
2018-05-01 09:46:23 -0700

    add more tests

--- app/__tests__/client-test.jsx
+++ app/__tests__/client-test.jsx
@@ -1,8 +1,27 @@
 import Index from '../client';
+import initialState from '../tests/helpers/initialState';
 
-test('renders without crashing', () => {
-  // expect(Object.assign({}, Index, { _reactInternalInstance: 'censored' })).toEqual(null);
-  expect(JSON.stringify(
-    Object.assign({}, Index, { _reactInternalInstance: 'censored' })
-  )).toMatchSnapshot();
+describe('client.jsx', () => {
+  let renderedClient;
+  let props;
+
+  const rendered = (state = initialState) => {
+    if (!renderedClient) {
+      global.window.__INITIAL_STATE__ = state;
+      renderedClient = Object.assign(
+        {}, Index, { _reactInternalInstance: 'censored' });
+    }
+    return renderedClient;
+  };
+
+  test('renders without crashing', () => {
+    // expect(Object.assign({}, Index, { _reactInternalInstance: 'censored' })).toEqual(null);
+    expect(JSON.stringify(
+      rendered()
+    )).toMatchSnapshot();
+  });
+
+  test('renders <Provider> passing store as props', () => {
+
+  });
 });
