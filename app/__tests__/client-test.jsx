e03859045ebdd8ab508633b2e9d8b0cc8facd190
joshweir <joshua.weir@gmail.com>
2018-05-02 10:38:22 -0700

    add more tests

--- app/__tests__/client-test.jsx
+++ app/__tests__/client-test.jsx
@@ -1,13 +1,22 @@
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import Index from '../client';
 import initialState from '../tests/helpers/initialState';
+import configureStore from '../store/configureStore';
+import createRoutes from '../routes';
 
 describe('client.jsx', () => {
   let renderedClient;
-  let props;
+  let store;
+  let history;
+  let routes;
 
   const rendered = (state = initialState) => {
     if (!renderedClient) {
       global.window.__INITIAL_STATE__ = state;
+      store = configureStore(initialState, browserHistory);
+      history = syncHistoryWithStore(browserHistory, store);
+      routes = createRoutes(store);
       renderedClient = Object.assign(
         {}, Index, { _reactInternalInstance: 'censored' });
     }
@@ -15,13 +24,25 @@ describe('client.jsx', () => {
   };
 
   test('renders without crashing', () => {
-    // expect(Object.assign({}, Index, { _reactInternalInstance: 'censored' })).toEqual(null);
-    expect(JSON.stringify(
-      rendered()
-    )).toMatchSnapshot();
+    expect(JSON.stringify(rendered())).toMatchSnapshot();
   });
 
   test('renders <Provider> passing store as props', () => {
+    expect(JSON.stringify(rendered().props.store))
+    .toEqual(JSON.stringify(store));
+  });
 
+  test('renders <Router> as children with props', () => {
+    const renderedChildren = rendered().props.children;
+    expect(renderedChildren.type).toEqual(Router);
+    expect(JSON.stringify(renderedChildren.props.history))
+    .toEqual(JSON.stringify(history));
+    renderedChildren.props.onUpdate();
+    expect(global.window.__INITIAL_STATE__).toEqual(null);
+  });
+
+  test('renders routes as children of <Router>', () => {
+    expect(JSON.stringify(rendered().props.children.props.children))
+    .toEqual(JSON.stringify(routes));
   });
 });
